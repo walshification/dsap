c7baaf83a5c31bdf7f6747a9ee6a8758d382fea8
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2022-01-16 13:34:13 -0600

    Solve R-1.4 with a comprehension and sum

---
+++ ch01/reinforcement/r_1_5.py
@@ -0,0 +1,11 @@
+"""
+Give a single command that computes the sum from Exercise R-1.4,
+relying on Python's comprehension syntax and the built-in sum function.
+"""
+
+
+def sum_of_smaller_squares(n: int) -> int:
+    """Returns the sum of the squares of all the positive integers
+    smaller than n.
+    """
+    return sum(num * num for num in range(n - 1, 0, -1))
