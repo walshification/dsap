ffd41cb211597ca725ba7604066c05e1347c6f5c
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2022-01-18 09:38:18 -0600

    I'll test this later

--- ch01/creativity/__init__.py
+++ ch01/creativity/__init__.py
@@ -2,3 +2,4 @@ from ch01.creativity.c_1_13 import reverse
 from ch01.creativity.c_1_14 import has_odd_product_pair
 from ch01.creativity.c_1_15 import are_distinct
 from ch01.creativity.c_1_20 import my_shuffle
+from ch01.creativity.c_1_21 import reverse_read
