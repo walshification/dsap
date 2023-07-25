237dbb4805783f62a7f0b48c69e9d37c3cd9026f
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2023-07-25 13:49:55 -0500

    Create chapter 6 data structures

---
+++ ch06/array_queue.py
@@ -0,0 +1,62 @@
+from typing import Any
+
+from ch06.empty_exception import Empty
+
+
+class ArrayQueue:
+    """FIFO queue implementation using a Python list as underlying storage."""
+    DEFAULT_CAPACITY = 10
+
+    def __init__(self) -> None:
+        """Create an empty queue."""
+        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+
+    def __len__(self) -> int:
+        """Return the number of elements in the queue."""
+        return self._size
+
+    def is_empty(self) -> bool:
+        """Return True if the queue is empty."""
+        return self._size == 0
+
+    def first(self) -> Any:
+        """Return (but do not remove) the element at the front of the queue.
+
+        Raise Empty exception if the queue is empty.
+        """
+        if self.is_empty():
+            raise Empty("Queue is empty.")
+        return self._data[self._front]
+
+    def dequeue(self) -> Any:
+        """Remove and return the first element of the queue (i.e., FIFO).
+
+        Raise Empty exception if the queue is empty.
+        """
+        if self.is_empty():
+            raise Empty("Queue is empty.")
+        answer = self._data[self._front]
+        self._data[self._front] = None
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
+
+    def enqueue(self, e: Any) -> None:
+        """Add an element to the back of the queue."""
+        if self._size == len(self._data):
+            self._resize(2 * len(self.data))
+        avail = (self._front + self.size) % len(self._data)
+        self._data[avail] = e
+        self._size += 1
+
+    def _resize(self, cap: int) -> None:
+        """Resize to a new list of capacity >= len(self)."""
+        old = self._data
+        self._data = [None] * cap
+        walk = self._front
+        for k in range(self._size):
+            self._data[k] = old[walk]
+            walk = (1 + walk) % len(old)
+        self._front = 0
